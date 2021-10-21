562760f88fae591ba6a91a9e1511bbcc8b092979
Francisco Javier Hinojosa Nieto <yobegdir@gmail.com>
2021-10-21 21:13:29 +0200

    quinto commit
    quinto commit

--- objetivo4/fase3.py
+++ objetivo4/fase3.py
@@ -11,3 +11,11 @@ while item1 < 3:
     item1 = item1 + 1
 
 print("")
+
+item1 = 0
+while item1 < 3:
+    item2 = 0
+    while item2 < 5:
+        print("item1 = " + str(item1) + ", item2 = " + str(item2))
+        item2 = item2 + 1
+    item1 = item1 +1
