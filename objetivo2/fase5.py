e54570cd00ada77184051c0f900a5df120ea0cc0
Francisco Javier Hinojosa Nieto <francisco.hinojosa@ineco.com>
2021-10-21 09:59:00 +0200

    tercer commit
    tercer commit

--- objetivo2/fase5.py
+++ objetivo2/fase5.py
@@ -26,11 +26,70 @@ print(cadenaejemplo.isalnum())
 
 print("")
 
-cadenaejemplo = "en un lugar de la mancha..."
-print(cadenaejemplo.isalnum())
+cadenaejemplo = "enunlugardelamancha"
+print(cadenaejemplo.isalpha())
 cadenaejemplo = "1234567890"
-print(cadenaejemplo.isalnum())
+print(cadenaejemplo.isalpha())
 cadenaejemplo = "abcdefg1234567890"
-print(cadenaejemplo.isalnum())
+print(cadenaejemplo.isalpha())
 cadenaejemplo = "abcdefg 1234567890"
-print(cadenaejemplo.isalnum())
+print(cadenaejemplo.isalpha())
+
+print("")
+
+cadenaejemplo = "en un lugar de la mancha"
+print(cadenaejemplo.isdigit())
+cadenaejemplo = "1234567890"
+print(cadenaejemplo.isdigit())
+cadenaejemplo = "abcdefg 1234567890"
+print(cadenaejemplo.isdigit())
+
+print("")
+
+cadenaejemplo = "En un lugar de la mancha"
+print(cadenaejemplo.islower())
+cadenaejemplo = "en un lugar de la mancha"
+print(cadenaejemplo.islower())
+
+print("")
+
+cadenaejemplo = "En un lugar de la mancha"
+print(cadenaejemplo.isupper())
+cadenaejemplo = "EN UN LUGAR DE LA MANCHA"
+print(cadenaejemplo.isupper())
+
+print("")
+
+cadenaejemplo = "en un lugar de la mancha"
+print(cadenaejemplo.lstrip())
+cadenaejemplo = "en un lugar de la mancha"
+print(cadenaejemplo.rstrip())
+cadenaejemplo = "en un lugar de la mancha"
+print(cadenaejemplo.strip())
+
+print("")
+
+cadenaejemplo = "abcdefghijklmnopqrstuvwxyz"
+print(max(cadenaejemplo))
+print(min(cadenaejemplo))
+
+print("")
+
+cadenaejemplo = "AEIOU"
+print(cadenaejemplo.replace('A','E'))
+
+print("")
+
+cadenaejemplo = "En un lugar de la mancha"
+print(cadenaejemplo.swapcase())
+
+print("")
+
+cadenaejemplo = "En un lugar de la mancha"
+print(cadenaejemplo.split())
+
+print("")
+
+cadenaejemplo = "31/12/20217"
+print(cadenaejemplo.split("/"))
+
